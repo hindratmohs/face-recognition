4d0a7f5471fb70bb7097ba3996d639377a672b61
Hindra_Hs <66427706+Hindra-HS@users.noreply.github.com>
2020-06-04 21:47:33 +0700

    upload file

---
+++ face.py
@@ -0,0 +1,60 @@
+import csv
+import cv2
+import os
+import numpy as np
+from PIL import Image
+import pickle
+
+
+
+Id = input("Enter Your Id: ")
+name = input("Enter Your Name: ")
+#membuat folder baru
+
+face_cascade = cv2.CascadeClassifier(r'C:\Users\Indra\Downloads\Kp\Project\cascades\data\haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+cap = cv2.VideoCapture(0) #menangkap object 
+sampleNum = 0
+
+
+while(True):
+    # Pengambilan bingkai(frame) demi bingkai
+    ret, frame = cap.read()
+    # Operasi pada frame datang ke sini
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        print(x,y,w,h)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        #incrementing sample number
+        sampleNum = sampleNum+1
+        cv2.imwrite("images\Dataset" + os.sep +name + "."+Id + '.' + str(sampleNum) + ".jpg", roi_gray) #menyimpan Gambar
+        print("Berhasil menyimpan")
+
+        #reconigize ?
+        color = (255, 0, 0) #BGR
+        stroke = 2
+        end_color_x = x + w
+        end_color_y = y + h
+
+        cv2.rectangle(frame, (x,y), (end_color_x,end_color_y), color, stroke)
+        
+    # Tampilkan frame yang dihasilkan 100 milisecond
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+    # break if the sample number is morethan 100
+    elif sampleNum > 25:
+        break
+# When everything done, release the capture
+cap.release() #menutup kembali kamera
+cv2.destroyAllWindows()
+res = "Images Saved for ID : " + Id + " Name : " + name
+row = [Id, name]
+with open("Mahasiswa"+os.sep+"mahasiswa.csv", 'a+') as csvFile:
+    writer = csv.writer(csvFile)
+    writer.writerow(row)
+csvFile.close()
+
+
